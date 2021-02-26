933f3587be80b3b3ae99ad3f1dbdbc9fd76d91c4
kraiponn <kraipon.na10@gmail.com>
2021-02-26 19:52:32 +0700

    init project and added library

---
+++ app/src/main/java/com/codemakerlab/newsapi/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.codemakerlab.newsapi.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codemakerlab.newsapi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
