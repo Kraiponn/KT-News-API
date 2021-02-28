8bd51a828f01908274aff83f47b5d1e99de5e528
kraiponn <kraipon.na10@gmail.com>
2021-02-28 11:12:10 +0700

    add paginate to recyclerView

--- app/src/main/java/com/codemakerlab/newsapi/presentation/view/fragment/NewsHeadLinesFragment.kt
+++ app/src/main/java/com/codemakerlab/newsapi/presentation/view/fragment/NewsHeadLinesFragment.kt
@@ -1,13 +1,16 @@
 package com.codemakerlab.newsapi.presentation.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.codemakerlab.newsapi.R
 import com.codemakerlab.newsapi.data.model.APIResponse
 import com.codemakerlab.newsapi.databinding.FragmentNewsHeadLinesBinding
@@ -22,6 +25,10 @@ class NewsHeadLinesFragment : Fragment() {
     private lateinit var binding: FragmentNewsHeadLinesBinding
     private var country = "us"
     private var page = 1
+    private var isScrolling = false
+    private var isLoading = false
+    private var isLastPage = false
+    private var pages = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,7 +55,16 @@ class NewsHeadLinesFragment : Fragment() {
                     is Resource.Success -> {
                         hideProgressBar()
                         resource.data?.let { apiResponse: APIResponse ->
+                            Log.d("MyTag", "Came here ${apiResponse.articles.toList().size}")
                             newsAdapter.differ.submitList(apiResponse.articles.toList())
+
+                            pages = if (apiResponse.totalResults % 20 == 0) {
+                                apiResponse.totalResults / 20
+                            } else {
+                                apiResponse.totalResults / 20 + 1
+                            }
+
+                            isLastPage = page == pages
                         }
                     }
                     is Resource.Loading -> {
@@ -72,15 +88,43 @@ class NewsHeadLinesFragment : Fragment() {
         binding.rvNews.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@NewsHeadLinesFragment.onScrollListener)
         }
     }
 
     private fun showProgressBar() {
+        isLoading = true
         binding.progressBar.visibility = View.VISIBLE
     }
 
     private fun hideProgressBar() {
+        isLoading = false
         binding.progressBar.visibility = View.INVISIBLE
+    }
+
+    private val onScrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val layoutManager = binding.rvNews.layoutManager as LinearLayoutManager
+            val sizeOfTheCurrentList = layoutManager.itemCount
+            val visibleItems = layoutManager.childCount
+            val topPosition = layoutManager.findFirstVisibleItemPosition()
+
+            val hasReachedToEnd = topPosition + visibleItems >= sizeOfTheCurrentList
+            val shouldPaginate = !isLoading && !isLastPage && hasReachedToEnd && isScrolling
+            if(shouldPaginate) {
+                page++
+                viewModel.getNewsHeadLines(country, page)
+                isScrolling = false
+            }
+        }
     }
 
 }
